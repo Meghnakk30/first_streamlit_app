77774a65bb630b66b6a7c661ca03eee568608e89
Meghnakk30 <140590436+Meghnakk30@users.noreply.github.com>
2023-07-27 11:28:47 +0530

    Update streamlit_app.py
    added menu 
    added menu iteams

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.title('Omega 3 and blueberry oatmeal')
+streamlit.title('Kale, Spinach and Rocket smoothie ')
+streamlit.title('Hard-Boiled Free-Range Eggs')
